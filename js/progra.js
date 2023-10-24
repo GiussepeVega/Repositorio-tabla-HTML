a8de08d2982cae86b45ae5f15898d8ff8c9c1348
GiussepeVega <GiussepeVega@users.noreply.github.com>
2023-10-24 10:20:14 -0500

    agregue codigo para cambiar color

--- js/progra.js
+++ js/progra.js
@@ -1,4 +1,21 @@
 
+cambiarColor()
+
 let name = prompt("Ingresa tu nombre")
 
 alert("Hola " + name + " bienvenido a esta pagina")
+
+
+function cambiarColor(){
+    
+
+let a = Math.floor(Math.random() * 256);
+let b = Math.floor(Math.random() * 256);
+let c = Math.floor(Math.random() * 256);
+
+document.querySelector("h1").style.color="rgb(" + a + "," + b + "," + c + ")";
+
+setTimeout("cambiarColor()", 1000);
+
+}
+
